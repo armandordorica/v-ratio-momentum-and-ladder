6ad9461660508f70681bf5a71c3179f591ef76fb
Omar-Elmofty <omar.elmofty31@gmail.com>
2020-08-09 10:24:38 -0400

    Add vratio to ranking system

--- RotationalMomentumWFreqFunc.py
+++ RotationalMomentumWFreqFunc.py
@@ -10,9 +10,10 @@ import detrendPrice
 import WhiteRealityCheckFor1
 import yfinance as yf
 from helper import *
+import functions as ff
 yf.pdr_override() 
 
-def rotational_momentum(lookback,shtrm_weight,RSI_weight, include_cash, verbose):
+def rotational_momentum(lookback,shtrm_weight,RSI_weight,v_ratio_weight, include_cash, verbose):
 
     #Hyper Parameters
     csv_name = "DIA.TLT"
@@ -22,6 +23,8 @@ def rotational_momentum(lookback,shtrm_weight,RSI_weight, include_cash, verbose)
     LongTermWeight = (1-shtrm_weight)
     ShortTermVolatilityWeight = .0
     Delay = 2
+    z_score_weight = 0.0
+    p_value_weight = 0.0
 
     #experimental cash filter
     Filter = 0
@@ -47,6 +50,7 @@ def rotational_momentum(lookback,shtrm_weight,RSI_weight, include_cash, verbose)
     dfA = dfP.drop(labels=None, axis=1, columns=dfP.columns)
     dfB = dfP.drop(labels=None, axis=1, columns=dfP.columns)
     dfS = dfP.drop(labels=None, axis=1, columns=dfP.columns)
+    dfVR = pd.DataFrame(columns=dfP.columns, index=['vratio','zscore', 'pvalue'])
     
     dfMA = dfP.drop(labels=None, axis=1, columns=dfP.columns)
     dfRSI = compute_rsi(dfP, n=14)
@@ -63,7 +67,12 @@ def rotational_momentum(lookback,shtrm_weight,RSI_weight, include_cash, verbose)
     for column in range(columns):
         dfS[dfP.columns[column]] = (dfR[dfP.columns[column]].rolling(window=Speriods).std())*math.sqrt(252)
         dfMA[dfP.columns[column]] = (dfP[dfP.columns[column]].rolling(window=MAperiods).mean())
-
+        vratio, zscore, pval = ff.vratio(np.array(dfP[dfP.columns[column]]), lag = Aperiods, cor = 'hom')
+        dfVR[dfP.columns[column]] = [vratio, zscore, pval]
+        
+    #Fill NAN values in dfVR
+    dfVR.fillna(1.0,inplace=True)
+    
     #Ranking each ETF w.r.t. short moving average of returns
     dfA_ranks = rank_assets(dfA, order=1)
     dfB_ranks = rank_assets(dfB, order=1)
@@ -71,7 +80,6 @@ def rotational_momentum(lookback,shtrm_weight,RSI_weight, include_cash, verbose)
     dfZ_ranks = rank_assets(dfZ, order=0)
     #dfRSI_ranks = rank_assets(dfRSI, order=0)
     
-
 
     #Weights of the varous ranks 
     dfA_ranks = dfA_ranks.multiply(ShortTermWeight) 
@@ -82,6 +90,10 @@ def rotational_momentum(lookback,shtrm_weight,RSI_weight, include_cash, verbose)
     dfAll_ranks = dfA_ranks.add(dfB_ranks, fill_value=0)
     dfAll_ranks = dfAll_ranks.add(dfS_ranks, fill_value=0)
     dfAll_ranks = dfAll_ranks.add(dfRSI_ranks, fill_value=0)
+    
+    for column in range(columns):
+        dfAll_ranks[dfP.columns[column]] = dfAll_ranks[dfP.columns[column]].values + (dfVR[dfP.columns[column]]['vratio'] - 1) * v_ratio_weight    
+    
     #dfAll_ranks = dfAll_ranks.add(dfZ_ranks, fill_value=0)
 
     #Choice is the dataframe where the ETF with the maximum score is identified
