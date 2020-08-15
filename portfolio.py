a8a62922be95b2de4aa03b8ae475add0877d38dd
Hao Yuan <kevin.yuan1995@gmail.com>
2020-08-15 13:05:28 -0400

    everything worked!
    Co-authored-by: Omar Elmofty <omar.elmofty31@gmail.com>

--- portfolio.py
+++ portfolio.py
@@ -1,17 +1,17 @@
 from os.path import isfile
 import yfinance as yf
 import pandas as pd
+import numpy as np
 import re
-from RotationalMomentumWFreqFunc import rotational_momentum
 import math
 import WhiteRealityCheckFor1
-from helper import getDate
 import matplotlib.pyplot as plt
 from matplotlib import style
-import numpy as np
-from computation_helper import *
-import pandas as pd
 import itertools
+
+from computation_helper import *
+from data_helper import *
+import rotational_momentum as rm
 
 
 class Portfolio:
@@ -52,7 +52,12 @@ class Portfolio:
 
         dfP = pd.read_csv(file_dir, parse_dates=['Date'])
         dfAP = pd.read_csv(ap_file_dir, parse_dates=['Date'])
-
+        dfP = dfP.sort_values(by='Date')
+        dfAP = dfAP.sort_values(by='Date')
+        dfP.set_index('Date', inplace = True)
+        dfAP.set_index('Date', inplace = True)
+        dfP['CASH'] = 1
+        dfAP['CASH'] = 1
         return dfP, dfAP
 
 
@@ -61,10 +66,10 @@ class Portfolio:
     def _parse_frequency(self, frequency_str):
 
         size = int(re.search("\d+%", frequency_str).group().strip("%"))
-        shift = int(int(re.match("\d+", frequency_str).group()) * size / 100)
-        unit = re.search("[A-Z]-[A-Z]", frequency_str).group()[0:-2]
+        shift_num = int(int(re.match("\d+", frequency_str).group()) * size / 100)
+        shift_unit = re.search("[A-Z]-[A-Z]*", frequency_str).group()
         freq = re.match("\d+[A-Z]-[A-Z]*", frequency_str).group()
-        return freq, str(shift)+unit, int(100/size)
+        return freq, shift_num, shift_unit, int(100/size)
 
 
     def _evaluate(self, df_list):
@@ -91,27 +96,33 @@ class Portfolio:
             #update DETREND_I
             dfPRR = dfPRR.assign(DETREND_I =np.cumprod(1+dfPRR['DETREND_ALL_R']))
             dfPRR.iat[0,dfPRR.columns.get_loc('DETREND_I')]= 1
-
+            print("\n==================Overall==================")
             dfPRR, TotaAnnReturn, CAGR, sharpe, volatility, p_val = evaluate(dfPRR)
 
 
         return dfPRR, TotaAnnReturn, CAGR, sharpe, volatility
 
     #entry point of the backtest
-    def backtest(self, datafile_alias, strategy, frequency, holding, rebalance_ratio=1):
+    def backtest(self, datafile_alias, lookback, Frequency, ShortTermWeight,
+                 LongTermWeight, RSI_weight, v_ratio_weight, Z_weight):
 
-        dfP, dfAP = self.load_data(datafile_alias)
+        dfP, dfAP = self._load_data(datafile_alias)
 
         #TODO: call _parse_frequency
-        freq, shift, n = self._parse_frequency(frequency)
+        freq, shift_num, shift_unit, n = self._parse_frequency(Frequency)
 
         #TODO: loop call the strategy execution function
         #to generate result files
         df_list = []
         for i in range(n):
             #use momentum trading here
-            df = rotational_momentum(dfP, dfAP, TBD)
-            file_dir = "Results/" + self.alias + "_" + str(i+1) + ".csv"
+            each_shift = str(shift_num * i) + shift_unit
+            df = rm.rotational_momentum(dfP, dfAP, lookback, freq, each_shift,
+                                        ShortTermWeight,
+                                        LongTermWeight, RSI_weight,
+                                        v_ratio_weight, Z_weight)
+
+            file_dir = "Results/" + datafile_alias + "_" + str(i+1) + ".csv"
             df.to_csv(file_dir, header = True, index=True)
             df_list.append(df)
 
@@ -121,7 +132,8 @@ class Portfolio:
         return df, TotaAnnReturn, CAGR, sharpe, volatility
 
     #TODO: call backtest to collect performance of each params combo
-    def grid_search(self, TBD):
+    def grid_search(self, datafile_alias, lookback, Frequency, ShortTermWeight,
+                 LongTermWeight, RSI_weight, v_ratio_weight, Z_weight):
 
         #create cartesian product of params list
 # =============================================================================
@@ -129,22 +141,54 @@ class Portfolio:
 #         grid = list(itertools.product([1, 2],[3, 4]))
 #         grid should contain (1,3), (1,4), (2,3), (2,4)
 # =============================================================================
+        results = []
+        grid = list(itertools.product(lookback, Frequency,
+                                      ShortTermWeight, LongTermWeight,
+                                      RSI_weight, v_ratio_weight, Z_weight))
 
-        #for each params combo,
-
+        for combo in grid:
+            print(combo)
             #call self.backtest
+            df, TotalAnnReturn, CAGR, sharpe, volatility\
+            = self.backtest(datafile_alias, combo[0], combo[1], combo[2], combo[3],
+                       combo[4], combo[5], combo[6])
+            output = [combo[0], combo[1], combo[2], combo[3],combo[4],
+                      combo[5], combo[6], TotalAnnReturn, CAGR,
+                      sharpe, volatility]
+            results.append(output)
 
-            #collect metrics
+        result_df = pd.DataFrame.from_records(data=results,
+                                              columns=["lookback",
+                                                       "frequency",
+                                                       "ShortTermWeight",
+                                                       "LongTermWeight",
+                                                       "RSI_weight",
+                                                       "v_ratio_weight",
+                                                       "Z_weight",
+                                                       "TotalAnnReturn",
+                                                       "CAGR",
+                                                       "sharpe",
+                                                       "volatility"])
 
-        #print and save metrics
-
+        file_dir = "Results/" + datafile_alias + "_gs" + ".csv"
+        result_df.to_csv(file_dir, header = True, index=True)
         return
 
-#example
-portfolio = Portfolio("SPY AAPL")
-portfolio.download_data("train", "2004-01-01", "2015-12-31")
-portfolio.backtest("train")
 # =============================================================================
-# portf = Portfolio("", datafile_alias="train")
-# print(portf._parse_frequency("4W-FRI-25%"))
+# #example
+# portfolio = Portfolio("SPY AAPL")
+# portfolio.download_data("train", "2004-01-01", "2015-12-31")
+# portfolio.backtest("train")
 # =============================================================================
+portf = Portfolio("AAPL, WMT, INTC, NFLX")
+#print(portf._parse_frequency("4W-FRI-25%"))
+portf.download_data("experiment6", "2004-01-01", "2008-12-31")
+#portf.backtest("experiment6", 20, "4W-FRI-25%", 1, 2, 1, 0, 0)
+portf.grid_search("experiment6",
+                  [10, 20],
+                  ["1W-FRI-100%", "2W-FRI-100%", "3W-FRI-100%", "4W-FRI-25%"],
+                  [1],
+                  [2],
+                  [1],
+                  [0],
+                  [0])
